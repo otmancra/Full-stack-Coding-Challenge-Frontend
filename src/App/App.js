1933b7eb57698d734655b9739113012fe964745b
Otman Craay <otmancr0@gmail.com>
2020-01-05 14:44:37 +0100

    making the project architecture, notFound and login interfaces

---
+++ src/App/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {
+  Route,
+  Switch
+} from 'react-router-dom';
+import AppHeader from '../common/AppHeader';
+import NotFound from '../common/NotFound';
+import Login from '../user/login/Login';
+import Alert from 'react-s-alert';
+import 'react-s-alert/dist/s-alert-default.css';
+import 'react-s-alert/dist/s-alert-css-effects/slide.css';
+import './App.css';
+import Home from '../home/Home';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authenticated: false,
+      currentUser: null,
+      loading: false
+    }
+  }
+
+  render() {
+    return (
+      <div className="app">
+          <div className="app-top-box">
+            <AppHeader authenticated={this.state.authenticated} onLogout={this.handleLogout} />
+          </div>
+          <div className="app-body">
+          <Switch>
+            <Route exact path="/" component={Home}></Route>  
+            <Route path="/login"
+            render={(props) => <Login authenticated={this.state.authenticated} {...props} />}></Route>         
+            <Route component={NotFound}></Route>
+          </Switch>
+        </div>
+        <Alert stack={{limit: 3}} 
+          timeout = {3000}
+          position='top-right' effect='slide' offset={65} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/user/login/Login.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import './Login.css';
+import { ACCESS_TOKEN } from '../../constants';
+import { login } from '../../util/APIUtils';
+import { Link, Redirect } from 'react-router-dom'
+import Alert from 'react-s-alert';
+
+class Login extends Component {
+    render() {
+
+        return (
+            <div className="login-container">
+                <div className="login-content">
+                    <h1 className="login-title">Login</h1>
+                    <LoginForm {...this.props} />
+                    <span className="signup-link">New user? <Link to="/signup">Sign up!</Link></span>
+                </div>
+            </div>
+        );
+    }
+}
+
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const inputName = target.name;        
+        const inputValue = target.value;
+
+        this.setState({
+            [inputName] : inputValue
+        });        
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();   
+
+        const loginRequest = Object.assign({}, this.state);
+        console.log('--- login : ', loginRequest)
+    }
+    
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-item">
+                    <input type="email" name="email" 
+                        className="form-control" placeholder="Email"
+                        value={this.state.email} onChange={this.handleInputChange} required/>
+                </div>
+                <div className="form-item">
+                    <input type="password" name="password" 
+                        className="form-control" placeholder="Password"
+                        value={this.state.password} onChange={this.handleInputChange} required/>
+                </div>
+                <div className="form-item">
+                    <button type="submit" className="btn btn-block btn-primary">Login</button>
+                </div>
+            </form>                    
+        );
+    }
+}
+
+export default Login
